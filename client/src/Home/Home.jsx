1356d4a24f68367b15f8b9c7791e61657486b2c7
gauravsondhiya <gauravsondhiya1503@gmail.com>
2025-05-23 23:27:00 +0530

    homepageupdate

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { assets } from "../assets/assets";
+import { assets, stepsData } from "../assets/assets";
 const Home = () => {
   return (
     <div className="w-[90%] m-auto border border-black text-center">
@@ -8,7 +8,7 @@ const Home = () => {
           Turn text to <br />
           <span className="text-blue-400">Image</span>, in seconds.
         </p>
-        <p className=" mt-4">
+        <p className="mt-4">
           Unleash your creativity with AI. Turn your imagination into visual art
           in seconds <br />
           Just type, and watch the magic happen.
@@ -20,13 +20,47 @@ const Home = () => {
         Generate Images
       </button>
 
-      <div className="mt-5 w-[543px] h-[77px] [&>img]:w-[77px] grid grid-cols-5 m-auto [&>img]:rounded-2xl" >
+      <div className="mt-5 w-[543px] h-[77px] [&>img]:w-[77px] grid grid-cols-5 m-auto [&>img]:rounded-xl">
         <img src={assets.sample_img_1} alt="" />
         <img src={assets.sample_img_2} alt="" />
         <img src={assets.sample_img_1} alt="" />
         <img src={assets.sample_img_2} alt="" />
         <img src={assets.sample_img_1} alt="" />
       </div>
+
+      <p className="text-3xl font-bold mt-9" >How it works</p>
+      <p>Transform Words Into Stunning Images</p>
+      <div>
+        {stepsData.map((e) => (
+          <div className=" rounded-2xl border-black w-[705px] h-[114px] m-auto flex text-left mt-3 shadow-md">
+            <div className="w-[10%]  m-auto">
+              <img className="w-[45px] m-auto" src={e.icon} alt="" />
+            </div>
+            <div className=" w-[80%]  border-red-400 m-auto -ml-4">
+              <p className="text-2xl font-semibold">{e.title}</p>
+              <p>{e.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+          {/* description */}
+
+          <p className="text-3xl mt-7 font-bold">Create AI Images</p>
+          <p className="text-xl">Turn your imagination into visuals</p>
+
+           <div className="w-[80%] m-auto  border-red-500 grid sm:grid-cols-2 gap-2 mt-5">
+              <div className="h-[100%]">
+                 <img src={assets.sample_img_1} alt="" />
+              </div>
+              <div className=" border-yellow-400 text-left">
+                <h1 className="font-semibold text-2xl">Introducing the AI-Powered Text to Image Generator</h1>
+                <br />
+                <p className="">Easily bring your ideas to life with our free AI image generator. Whether you need stunning visuals or unique imagery, our tool transforms your text into eye-catching images with just a few clicks. Imagine it, describe it, and watch it come to life instantly. <br /><br />
+                Simply type in a text prompt, and our cutting-edge AI will generate high-quality images in seconds. From product visuals to character designs and portraits, even concepts that don’t yet exist can be visualized effortlessly. Powered by advanced AI technology, the creative possibilities are limitless!
+                 </p>
+              </div>
+           </div>
     </div>
   );
 };
